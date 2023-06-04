4c81b90f5cadada49c469c3206748a422b38fb03
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-04 15:17:54 +0530

    Array List

---
+++ src/main/java/demo/Demo.java
@@ -0,0 +1,15 @@
+package demo;
+
+import java.util.ArrayList;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        ArrayList <String>strList=new ArrayList<>();
+        strList.add("A");
+        strList.add("B");
+        strList.add("C");
+        strList.add("D");
+        System.out.println(strList); //strList.toString()
+    }
+}
