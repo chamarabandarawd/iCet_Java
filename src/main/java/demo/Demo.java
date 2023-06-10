a90689f23d071acf3b86d782636b6cfdaf2c4cbf
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-10 15:21:14 +0530

    Thread run correctly with sleep

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,14 +1,16 @@
 import java.util.*;
 class Demo{
     public static void main(String args[]){
+        MyThread t1=new MyThread();
+        t1.start();
+
         for (int i = 0; i < 10; i++){
             System.out.println("Program A : "+i);
             try{Thread.sleep(500);}catch(InterruptedException ex){}
         }
         System.out.println("---------------");
 
-        MyThread t1=new MyThread();
-        t1.run();
+
     }
 }
 class MyThread extends Thread{
