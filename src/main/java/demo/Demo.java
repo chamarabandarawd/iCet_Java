654c7a93aabe6fa7c2f7ce93a2c7a87341eb2022
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-10 15:06:56 +0530

    Single task

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,22 +1,15 @@
-package demo;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
-
-public class Demo {
-    class Customer {
-        private int code;
-        private String name;
-
-        public Customer(int code, String name) {
-            this.code = code;
-            this.name = name;
+import java.util.*;
+class Demo{
+    public static void main(String args[]){
+        for (int i = 0; i < 10; i++){
+            System.out.println("Program A : "+i);
+            try{Thread.sleep(500);}catch(InterruptedException ex){}
         }
-    }
+        System.out.println("---------------");
+        for (int i = 0; i < 10; i++){
+            System.out.println("Program B : "+i);
+            try{Thread.sleep(500);}catch(InterruptedException ex){}
+        }
 
-
-        public static void main(String[] args) {
-            LinkedList<Customer> custQue = new LinkedList<>();
-            custQue.addLast(new Customer(1001,"Danapala"));
     }
 }
