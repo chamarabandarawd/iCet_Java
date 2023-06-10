d72086ef2ff90f7e30454605806860019fe25c6b
chamarabandarawd <chamarabandarawd@gmail.com>
2023-06-10 15:11:34 +0530

    try run tread, but wrong

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -6,10 +6,16 @@ class Demo{
             try{Thread.sleep(500);}catch(InterruptedException ex){}
         }
         System.out.println("---------------");
+
+        MyThread t1=new MyThread();
+        t1.run();
+    }
+}
+class MyThread extends Thread{
+    public void run(){
         for (int i = 0; i < 10; i++){
             System.out.println("Program B : "+i);
             try{Thread.sleep(500);}catch(InterruptedException ex){}
         }
-
     }
 }
